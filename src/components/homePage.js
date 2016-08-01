18752460f00d122929c6de23dcf9c2be97036cd1
PC324156 <saikat.bhattacharya@pearson.com>
2016-08-01 16:09:36 +0530

    Session-2

---
+++ src/components/homePage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+class home extends React.Component {
+  render = ()=>{
+    return(
+      <div>
+        <h1>OUR FIRST EVER REACT APP!</h1>
+        <p>Rendering from our FIRST component!</p>
+      </div>
+    )
+  }
+}
+
+export default home;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import Home from './components/homePage';
+
+ReactDom.render((<Home />),document.getElementById('app'));
