ad5ba0742a25cae94d3b29f3c33193331021541c
PC324156 <saikat.bhattacharya@pearson.com>
2016-08-01 19:45:49 +0530

    final demo

--- src/components/homePage.js
+++ src/components/homePage.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import style from '../css/style.css';
 
 
 class home extends React.Component {
   render = ()=>{
     return(
-      <div>
-        <h1>OUR FIRST EVER REACT APP!</h1>
-        <p>Rendering from our FIRST component!</p>
-      </div>
+      <div className = "container">
+	<div className="wrapper">
+		<form className="form-signin">
+		    <h3 className="form-signin-heading">Please Sign In</h3>
+			  <hr className="colorgraph"></hr><br />
+
+			  <input type="text" className="form-control" name="Username" placeholder="Username" required="" autofocus="" />
+			  <input type="password" className="form-control" name="Password" placeholder="Password" required=""/>
+
+			  <button className="btn btn-lg btn-primary btn-block" value="Login">Login</button>
+		</form>
+	</div>
+</div>
     )
   }
 }
